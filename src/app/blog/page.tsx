5dc3f62c64beb579182f54989d982df6e1d125d0
Adrian Viloria <adriank.viloria@gmail.com>
2025-11-03 18:04:03 +0800

    fix: Revert to 'any' type for blog pages to force build pass

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -60,6 +60,7 @@ export default async function BlogPage() {
                       className="aspect-[16/9] w-full rounded-2xl bg-gray-100 object-cover sm:aspect-[2/1] lg:aspect-[3/2]"
                     />
                   ) : (
+                    // Placeholder if no image
                     <div className="aspect-[16/9] w-full rounded-2xl bg-gray-100 sm:aspect-[2/1] lg:aspect-[3/2]" />
                   )}
                   <div className="absolute inset-0 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
